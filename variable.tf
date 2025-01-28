7074f29f594a00e5cd36b4cd613c85967237e742
CliffordCheung <cliffordcheung77@gmail.com>
2025-01-28 08:04:31 +0800

    Final code for terraform with creation of dynamodb and insertitem via aws_dynamotable_item

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "primary_key" {
+    type = string
+    description = "hash_key"
+    default = "ISDN"
+}
+variable "secondary_key" {
+    type = string
+    description = "range_key"
+    default = "Genre"
+}
+variable "db_table_name" {
+    type = string
+    description = "dynamodb table name"
+    default = "clifford-dynamodb-table2"
+  
+}
